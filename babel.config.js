c345b5b36d28473f20815a925fb51ad0a11a8013
stonkoshkur <sergiy.tonkoshkur@gmail.com>
2020-11-07 12:35:13 +0200

    chore(app): add babel-plugin-module-resolver for path aliases chore(prettier): update rules

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      /*
+       * Path aliases resolver
+       * https://github.com/tleunen/babel-plugin-module-resolver
+       */
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        root: ['./src'],
+        alias: {
+          src: './src'
+        }
+      }
+    ]
+  ]
 };
